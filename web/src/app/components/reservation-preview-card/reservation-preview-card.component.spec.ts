1db27b60f041da2e7ba727ec9652a125e78b8bb5
fherborn <florianherborn2293@t-online.de>
2020-04-14 17:52:29 +0200

    Meine Buchungen implementiert

---
+++ web/src/app/components/reservation-preview-card/reservation-preview-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReservationPreviewCardComponent } from './reservation-preview-card.component';
+
+describe('ReservationPreviewCardComponent', () => {
+  let component: ReservationPreviewCardComponent;
+  let fixture: ComponentFixture<ReservationPreviewCardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ReservationPreviewCardComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReservationPreviewCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
