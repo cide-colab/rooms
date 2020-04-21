7df697578a7e03bdca3a96c5e4559354f6022945
fherborn <florianherborn2293@t-online.de>
2020-04-21 07:44:06 +0200

    Abteilungs Liste ausgelagert in eine eigene Komponente

--- web/src/app/components/department/department-list-item/department-preview-card.component.spec.ts
+++ web/src/app/components/department/department-list-item/department-preview-card.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { DepartmentPreviewCardComponent } from './department-preview-card.component';
+import {DepartmentPreviewCardComponent} from './department-preview-card.component';
 
 describe('DepartmentListItemComponent', () => {
   let component: DepartmentPreviewCardComponent;
